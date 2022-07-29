aa82490fa62a5c8b28b2596d803cdd6d8ce3ff88
tehtko <moderndsnsam@gmail.com>
2022-07-29 18:55:59 -0400

    Add project files.

---
+++ Emray/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Emray;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Emray/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Emray;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
