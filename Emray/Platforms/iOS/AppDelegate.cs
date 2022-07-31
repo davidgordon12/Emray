a884b8b1add7254e3330fc30dbcdd250ba451346
tehtko <moderndsnsam@gmail.com>
2022-07-30 20:10:48 -0400

    Remove unsupported platforms

--- Emray/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace Emray;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
